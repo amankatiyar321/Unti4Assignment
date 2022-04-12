c771491c4b73a9581293160f2835d6743d5f381c
Aman katiyar <97542315+amankatiyar321@users.noreply.github.com>
2022-04-13 00:31:39 +0530

    Add files via upload

---
+++ Staticpage/src/day1/List.jsx
@@ -0,0 +1,28 @@
+
+const  List=()=> {
+return  <div >
+
+   <h1>Mobile Operating System</h1>
+   <div>
+       <li>
+           <ul>Android</ul>
+           <ul>Sumsung</ul>
+           <ul>I-Phone</ul>
+           <ul>Window Phone</ul>
+       </li>
+   </div>
+   <h1>Mobile Manufacturers</h1>
+   <div>
+       <ul>
+           <li>Sumsung</li>
+           <li>HTC</li>
+           <li>Micromax</li>
+           <li>Apple</li>
+       </ul>
+   </div>
+
+  </div>
+
+}
+
+export default List
